10d6075f55cdbce9084d1dab1981ffbe9c9014db
Nataly <natasha.totska@yahoo.com>
2021-12-20 16:57:10 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -266,7 +266,7 @@ refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  document.body.classList.toggle("modal-open");
+  document.body.classList.toggle('modal-open');
   refs.modal.classList.toggle('is-hidden');
 }
 },{}],"index.js":[function(require,module,exports) {
@@ -307,7 +307,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64645" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62979" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
